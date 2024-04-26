994a82f427147621408076cddca2dbd19c8a0c19
leoszm <leonardosousa60@gmail.com>
2024-04-26 19:01:45 -0300

    finalizada sidebar com efeito, mudança de cor quando hover e fotos de perfil, fundo e mudança de posição

--- FirstProject/01-fundamentals-reactjs/src/components/Sidebar.jsx
+++ FirstProject/01-fundamentals-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -7,13 +9,16 @@ export function Sidebar() {
                 className={styles.cover}
                 src="images.unsplash.com/photo-1529074792710-b10868a90af1?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
+
             <div className={styles.profile} >
+                <img className={styles.avatar} src="https://github.com/leoszm.png" />
                 <strong>Diego Fernandes</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar ser perfil
                 </a>
             </footer>
